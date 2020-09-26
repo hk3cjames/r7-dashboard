fd4e071225b80c5de21d4566e8781c1b180d010f
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-09-26 23:10:42 +0800

    update with hash change

--- dash.js
+++ dash.js
@@ -1,28 +1,47 @@
 var express = require("express");
 var app = express();
+const fetch = require("node-fetch");
 
-app.set('view engine', 'ejs');
-// let d1 = '2020-06-30_12:33:45'
-// let d2 = '123,456'
-// let d3 = '312'
-// let d4 = '2141'
-// let d5 = '342454'
-// let d6 = '86'
-// let d7 = '98798'
-// let d8 = 'abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890'
+let d1 = "2020-06-30_12:33:45"; //time
+let d2 = 0; //cycle counter
+let d3 = 0; // active PS
+let d4 = 0; // hash in this cycle
+let d5 = 0; // hash in data base
+let d6 = 0;
+let d7 = 0;
+let d8 = "abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890";
+var data = { d1, d2, d3, d4, d5, d6, d7, d8 };
 
-let d1 = '2020-06-30_12:33:45'
-let d2 = '123,456'
-let d3 = '312'
-let d4 = '214901'
-let d5 = '342454'
-let d6 = '86'
-let d7 = '98798'
-let d8 = 'abcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890'
+app.listen(3700, () =>
+  console.log("dashboard server-1001 listening at localhost(or https):3700")
+);
+app.use(express.static("dashboard"));
+app.use(express.json({ limit: "1mb" }));
 
-app.get('/', function(req, res){
-    var data = {d1,d2,d3,d4,d5,d6,d7,d8}
-  res.render('profile', {data: data})
-})
+var res1 = {};
 
-app.listen(3000, console.log("rbas dashboard listening at port 3000"));
+app.get("/status", async (req, res) => {
+  rxjson = req.body;
+  console.log(rxjson);
+  console.log(data);
+  res.json(data);
+
+});
+
+var crypto = require("crypto");
+async function intervalFunc() {
+
+  data.d1 = Date.now()
+  data.d2++;
+  data.d4 = data.d4 +7;
+  if (data.d4 > 20) data.d4 = data.d4-20;
+  data.d5 = data.d5 + data.d4;
+  data.d7 = data.d7 + 3;
+  hash_data = "" + data.d5
+  data.d8 = crypto
+  .createHash("sha256")
+  .update(hash_data)
+  .digest("hex");
+}
+
+setInterval(intervalFunc, 1000);
